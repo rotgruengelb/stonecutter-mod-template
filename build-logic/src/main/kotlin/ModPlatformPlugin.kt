5346d0009170caa4dd6c8666a300751e98af55e4
Daniel (rotgruengelb) <rotgruengelb@rotgruengelb.net>
2026-01-16 18:03:06 +0100

    fix: stop override the version and fix legacy forge mixin file reference
    (cherry picked from commit 727c420c54a827d8e67144ebf95dc21568329232)

--- build-logic/src/main/kotlin/ModPlatformPlugin.kt
+++ build-logic/src/main/kotlin/ModPlatformPlugin.kt
@@ -183,7 +183,7 @@ abstract class ModPlatformPlugin @Inject constructor() : Plugin<Project> {
 			archiveBaseName.set(modId)
 			if (isForge) {
 				manifest.attributes(
-					"MixinConfigs" to "mixins.mymod.json"
+					"MixinConfigs" to "mixins.${modId}.json"
 				)
 			}
 		}
@@ -307,9 +307,6 @@ abstract class ModPlatformPlugin @Inject constructor() : Plugin<Project> {
 			withJavadocJar()
 			sourceCompatibility = requiredJava
 			targetCompatibility = requiredJava
-			toolchain {
-				version = requiredJava
-			}
 		}
 	}
 
