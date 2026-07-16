8d16b4eb4a1f77ae8d36627cb6df97b7384a6f1a
doonv <58695417+doonv@users.noreply.github.com>
2026-07-17 00:47:32 +0300

    fix incorrect version for selecting java version

--- build-logic/src/main/kotlin/Context.kt
+++ build-logic/src/main/kotlin/Context.kt
@@ -62,7 +62,7 @@ class Context(
 	val javaVersion: JavaVersion by lazy {
 		when {
 			stonecutter.eval(currentMcVersion, ">=26") -> JavaVersion.VERSION_25
-			stonecutter.eval(currentMcVersion, ">=1.20.6") -> JavaVersion.VERSION_21
+			stonecutter.eval(currentMcVersion, ">=1.20.5") -> JavaVersion.VERSION_21
 			stonecutter.eval(currentMcVersion, ">=1.18") -> JavaVersion.VERSION_17
 			stonecutter.eval(currentMcVersion, ">=1.17") -> JavaVersion.VERSION_16
 			else -> JavaVersion.VERSION_1_8
